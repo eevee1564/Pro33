9cd105c29572fc84d58688216cab3ae0221388f7
eevee1564 <78247125+eevee1564@users.noreply.github.com>
2021-04-29 15:40:04 +0530

    Add files via upload

---
+++ pro33/SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,17 @@
+var bg;
+
+function preload(){
+
+bg=loadImage("snow3.jpg");
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
---
+++ pro33/SnowFalling-C33-Template-main/snow (2).js
@@ -0,0 +1,12 @@
+class snow{
+
+    constructor(bodyA, pointB){
+        var options={
+            restitution:0.8,
+            density:1.4,
+            friction:0.3
+        }
+    }
+    boy=velocityY(5);
+
+}
